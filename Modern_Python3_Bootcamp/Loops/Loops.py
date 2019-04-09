f9c7833ae331e79ce1554b016abfa2eb64b5674e
mikaelbeat <ryynanenphm@gmail.com>
2019-04-09 16:32:16 +0300

    Update

---
+++ Modern_Python3_Bootcamp/Loops/Loops.py
@@ -0,0 +1,27 @@
+
+for letter in "Coffee":
+    print(letter)
+    
+    
+print("\n***************************************\n")
+    
+    
+list = ["Monkey", 22, "Metal Gear Solid", 6000]
+
+
+for i in list:
+    print(i)
+
+    
+print("\n***************************************\n")
+
+    
+for i in range(1,10):
+    print(i)
+
+
+print("\n***************************************\n")  
+
+  
+for i in range(1, 20, 2):
+    print(i)
--- Modern_Python3_Bootcamp/Rock_papers_scissors/Rock_papers_scissor.py
+++ Modern_Python3_Bootcamp/Rock_papers_scissors/Rock_papers_scissor.py
@@ -1,13 +1,36 @@
-p1 = input("Player 1: rock, paper, or scissors ")
-p2 = input("Player 2: rock, paper, or scissors ")
- 
-if p1 == p2:
-    print("Draw")
-elif p1 == "rock" and p2 == "scissors":
-    print("p1 wins")
-elif p1 == "paper" and p2 == "rock":
-    print("p1 wins")
-elif p1 == "scissors" and p2 == "paper":
-    print("p1 wins")
+
+from random import randint
+
+randomNumber = randint(1,3)
+
+if (randomNumber == 1):
+    p2 = "rock"
+elif (randomNumber == 2):
+    p2 = "paper"
 else:
-    print("p2 wins")
+    p2 = "scissors"
+    
+
+print("\n ********** ROCK PAPER SCISSORS & SUPER COMPUTER **********\n")
+p1 = input("Player 1: rock, paper, or scissors: ")
+p1 = p1.lower()
+
+
+if ((p1 == "rock") or (p1 == "paper") or (p1 == "scissors")):  
+    if p1 == p2:
+        print("\nComputer played " + str(p2))
+        print("\nDraw")
+    elif p1 == "rock" and p2 == "scissors":
+        print("\nComputer played " + str(p2))
+        print("\nYou win!")
+    elif p1 == "paper" and p2 == "rock":
+        print("\nComputer played " + str(p2))
+        print("\nYou win!")
+    elif p1 == "scissors" and p2 == "paper":
+        print("\nComputer played " + str(p2))
+        print("\nYou win!")
+    else:
+        print("\nComputer played " + str(p2))
+        print("\nConputer wins!")
+else:
+    print("\nInvalid input!")
