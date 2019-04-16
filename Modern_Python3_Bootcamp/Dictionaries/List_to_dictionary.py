afb780ce0b3b28994dc70aca2d3639853d695e1e
Petri Ryynänen <Petri Ryynnen@192.168.0.28>
2019-04-16 17:57:58 +0300

    Update

---
+++ Modern_Python3_Bootcamp/Dictionaries/List_to_dictionary.py
@@ -0,0 +1,11 @@
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+answer = dict(person)
+
+print(answer)
+
+solution = dict.fromkeys("aeiou", 0)
+
+print(solution)
---
+++ Modern_Python3_Bootcamp/Tuples_and_sets/Preview.py
@@ -0,0 +1,14 @@
+
+alphabet = ("a", "b", "c", "d", "a")
+
+print("\n********** Looping tuples **********\n")
+for value in alphabet:
+    print(value)
+    
+    
+print("\n********** Counting tuples values **********\n")
+print(alphabet.count("a"))
+
+
+print("\n********** Returns index tuples values **********\n")
+print(alphabet.count("a"))
