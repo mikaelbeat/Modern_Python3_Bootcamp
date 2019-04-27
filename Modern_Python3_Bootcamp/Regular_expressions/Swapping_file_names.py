1ec074b034b6a034590d23d3a3f1a97d5890860d
mikaelbeat <ryynanenphm@gmail.com>
2019-04-27 17:18:15 +0300

    Update

---
+++ Modern_Python3_Bootcamp/Regular_expressions/Swapping_file_names.py
@@ -0,0 +1,27 @@
+
+import re
+
+titles = [
+    "Significant Others (1987)",
+    "Tales of the City (1978)",
+    "The Days of Anna Magrigal (2014)",
+    "Mary Ann in Autumn (2010)",
+    "Further Tales of the City (1982)",
+    "Babycakes (1984)",
+    "More Tales of the City (1980)",
+    "Sure of You (1989)",
+    "Michael Tolliver Lives (2007)"
+    ]
+
+
+pattern = re.compile(r"(?P<title>^[\w ]+) \((?P<date>\d{4})\)")
+
+fixed_titles = []
+
+for book in titles:
+    #result = pattern.sub("\g<2> - \g<1>", book)
+    result = pattern.sub("\g<date> - \g<title>", book)
+    fixed_titles.append(result)
+    
+fixed_titles.sort()
+print(fixed_titles)
