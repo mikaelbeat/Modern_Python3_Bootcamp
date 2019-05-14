1110e17924b2139044d6054d31d88cde09c0cb43
mikaelbeat <ryynanenphm@gmail.com>
2019-05-14 16:22:49 +0300

    Huge update

--- Modern_Python3_Bootcamp/HTTP_requests/First_Python_request.py
+++ Modern_Python3_Bootcamp/HTTP_requests/First_Python_request.py
@@ -5,6 +5,11 @@ url = "http://www.google.com"
 
 response = requests.get(url)
 
+print("\n***** Printing response *****\n")
 print(response)
+
+print("\n***** Printing response status code *****\n")
 print(response.status_code)
+
+print("\n***** Printing response text *****\n")
 print(response.text)
